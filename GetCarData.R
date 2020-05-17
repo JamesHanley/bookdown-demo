dec69db34f904ed0e89138df46bd443b29e6a76f
JamesHanley <james.hanley@mcgill.ca>
2020-05-17 18:17:06 -0400

    update

--- GetCarData.R
+++ GetCarData.R
@@ -1,5 +1,12 @@
 
 setwd("/Users/jameshanley/Dropbox/Courses/statbook/")
+d1 = read.table("First31.txt",as.is=TRUE)
+d2 = read.table("Last3352.txt",as.is=TRUE)
+d = rbind(d1,d2)
+str(d)
+summary(d)
+head(d)
+
 
 ds=read.csv("veh0126.csv",as.is=TRUE,skip=6)
 head(ds[,1:10],6)
@@ -10,21 +17,44 @@ sum(ds$Total)
 fr = aggregate(ds$Total,by=list(Main=ds$Main),sum) ; str(fr)
 TOP = fr$Main[fr$x>10000]
 
-dimnames(ds)
+ds = ds[ds$Main %in% TOP,c(1,2,125,126)]
+str(ds)
 
-# str(ds)
-# head(ds[14000:14100,1:8],100)'
+Fr = aggregate(ds$Total,by=list(Main=ds$Generic.model.1),sum) ; 
+Fr = Fr[order(-Fr$x),] ; str(Fr)
+Fr = Fr[ Fr$x >= 1000, ] ; head(Fr)
 
-sort(table(ds$Make))
+Fr = Fr[ order(Fr$Main), ] ;
+Fr$Main = gsub("VAUXHALL", "OPEL", Fr$Main)
+ 
+head(Fr)
+head(d)
+
+Fr$Length = NA
+
+for(i in 1:length(Fr$Main)){
+	
+	hits = grep(Fr$Main[i],d$Model,ignore.case=TRUE)
+	if(length(hits) > 0 ) Fr$Length[i] = round(median(d$Length[hits]))	
+}
+Fr = Fr[!is.na(Fr$Length),]
+names(Fr)[1:2] = c("Make.Model","No.Registered")
+str(Fr)
+head(Fr)
+tail(Fr)
+sum(Fr$No.Registered)
+
+summary(Fr)
+hist(Fr$Length,breaks=25)
 
 
 
-head(ds); tail(ds)
+###########
 
-length(dimnames(ds)[[2]])
-Y=4:103
-t=apply(ds[,Y],2,sum,na.rm=TRUE)
 
+ALREADY=TRUE
+
+if(!ALREADY) {
 
 library(RCurl)
 
@@ -43,35 +73,47 @@ for(i in 1:n){
 	CO[i]=strsplit(strsplit(company[i],">")[[1]][2],"<")[[1]][1]
 }
 
-CO[c(5,9,11,12,14,15,19,20,22,23,27,29,32,34,35,37,38,40,
-   44,46,47,52,55,58,59,62,64,65,67,68,70,71,72,73,74,
-   75,76,79,80,82,83,84,85,87,88,89)]
+m = matrix(NA,length(Fr$Main),length(CO))
+for(i in 1:length(Fr$Main)){
+    for( j in 1:length(CO)){
+    	    hit = grep(CO[j],Fr$Main[i],ignore.case =TRUE)
+    	    if(length(hit)==1) m[i,j] = 1
+    }		
+}
 
+h = apply(m,1,sum,na.rm=TRUE)
+Fr = Fr[h>0,] ; str(Fr)
 
+H = apply(m,2,sum,na.rm=TRUE)
+Make.Index = (1:length(CO))[H>0]
+
+company = company[Make.Index]
+
+n = length(company)
 
 url1 =rep(NA,n)
 
-MODEL= rep(NA,10000)
-LENGTH=rep(NA,10000)
+MODEL= rep(NA,100000)
+LENGTH=rep(NA,100000)
 
 ii = 0
 
-for( i in c(5,9)){
+for( i in 33:n){
 	
+	print(i)
 	Txt = strsplit(company[i], " rel=")[[1]][1]
 	nk = nchar(Txt)
 	url1[i] = substr(Txt,2,nk-1)
-	#print(noquote(""))
-	#print(url1[i])
-	#print(noquote(""))
 	TXT2 = getURL(url1[i])
     txt2=strsplit(TXT2,"\n")[[1]]; length(txt2)
     txt2=txt2[grep("col-4",txt2)]
     txt2 = txt2[ grep(url1[i],txt2) ]
     
     n2 = length(txt2)
+    print(c("n2",n2))
     url2 =rep(NA,n2)
-    for( j in 1:2){
+    for( j in 1:n2){
+    	   #print(j)
     	   Txt2 = strsplit(txt2[j], " title=")[[1]][1]
     	   nk2 = nchar(Txt2)
     	   url2[j] = substr(Txt2,38,nk2-1)
@@ -84,20 +126,22 @@ for( i in c(5,9)){
        #print(txt3)
        #print(noquote(""))
        n3 = length(txt3)
+       #print(n3)
        url3 =rep(NA,n3)
-       for( k in 1:2){
+       for( k in ceiling( median(1:n3) ) ){
+       	     #print(k)
     	         Txt3 = strsplit(txt3[k], " title=")[[1]][1]
     	         Txt3 = strsplit(Txt3, "href=")[[1]][2]
     	         nk = nchar(Txt3)
     	         url3[k] = substr(Txt3,2,nk-1)
-             #print(url3[k])
+
              
              TXT4 = getURL(url3[k])
              txt4=strsplit(TXT4,"\n")[[1]];
              txt4 = txt4[grep("-specs/",txt4)]
              n4 = length(txt4)
              url4 =rep(NA,n4)       
-             for( m in 1:n4){	
+             for( m in ceiling( median(1:n4) ) ){	
              	Line = strsplit(txt4[m], " title=")[[1]]
              	model = Line[2] 
              	model = strsplit(model,">")[[1]][2]
@@ -106,8 +150,7 @@ for( i in c(5,9)){
     	        Txt4 = strsplit(Txt4, "href=")[[1]][2]
     	        nk = nchar(Txt4)
     	        url4[m] = substr(Txt4,2,nk-1)
-                #print(url4[m])
-                
+                #print(url4[m])           
                 TXT5 = getURL(url4[m])
                 txt5=strsplit(TXT5,"\n")[[1]];
                 
@@ -123,10 +166,10 @@ for( i in c(5,9)){
                 LENGTH[ii] = Txt
                 #print(url4[m])
                 #print(txt)
-                if( (ii %% 100) == 0 ) print(ii)         	             	
+                if( (ii %% 10) == 0 ) print(ii)         	             	
              } # m                  
     	   } # k
-    	   print(noquote(""))
+    	   #print(noquote(""))
     }  # j
 } # i
 
@@ -137,16 +180,6 @@ ds = data.frame(Model=MODEL,
 str(ds)
 ds = ds[!is.na(ds$Length),]
 str(ds)
-summary(ds$Length)
-table(ds$Length)
-hist(ds$Length)
 
-tail(ds)
-head(ds)
+} # ALREADY
 
-length(unique(ds$Model))
-
-setwd("/Users/jameshanley/Dropbox/Courses/Statbook")
-write.table(ds,"AudiBMW.txt")
-
-hist(unique(ds$Length))
